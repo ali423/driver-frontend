9143d7cb69ffb1903e2ba1e1028d804f81c28d3c
shaghayegh <shaghayegh.ahmadi@zoodfood.com>
2024-01-18 16:00:20 +0330

    add hexagon

--- src/app/component/map/index.tsx
+++ src/app/component/map/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
-import map from './../../../../public/images/map.png'
+import map from "./../../../../public/images/map1.png";
 
 const Map = () => {
-  return <div className={styles.mapContainer}><Image src={map} alt="map"/></div>;
+  let hexagon = () => {
+    return (
+      <div>
+        {Array.from(Array(12), (e, i) => {
+          return (
+            <div className={`${styles.hexagon} ${styles['id'+i]}`} key={i}>
+              {i}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+  return <div className={styles.mapContainer}><Image src={map} alt="map"/>{hexagon()}</div>;
 };
 
 export default Map;
