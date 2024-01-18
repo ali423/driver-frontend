9fb7af449430e3a75db01e868593e057fb6d0091
shaghayegh <shaghayegh.ahmadi@zoodfood.com>
2024-01-18 21:39:43 +0330

    add score

---
+++ src/app/component/score/index.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import styles from "./style.module.scss";
+import Coin from "./../../../../public/images/coint.jpg";
+import { formatPrice, toPersian } from "@/app/function";
+import Image from "next/image";
+
+const Score: FC = () => {
+  return (
+    <div className={styles.scoreContainer}>
+      <p>امتیازهای کسب شده</p>
+      <div className={styles.score}>
+        <p>۰</p>
+        <p>۴۷</p>
+        <p>۱۰۰</p>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.description}>
+        <Image src={Coin} alt="coin" />
+        <p>
+          در این هفته، تا امروز ۲۴۷ سکّه کسب کردید و ۲۰۰ هزار تومان به کیف
+          پول‌تان اضافه شد.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Score;
