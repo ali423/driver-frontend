e7fa47627d32e9129f6418db81350ef0e9220c5e
shaghayegh <shaghayegh.ahmadi@zoodfood.com>
2024-01-19 11:22:12 +0330

    set data of map

--- src/app/component/map/index.tsx
+++ src/app/component/map/index.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { FC } from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
 import map from "./../../../../public/images/map1.png";
 
-const Map = () => {
+interface IMap {
+  OriginAreaId: number;
+  DestinationAreaId: number;
+}
+
+const Map: FC<IMap> = ({ OriginAreaId, DestinationAreaId }) => {
   let hexagon = () => {
     return (
       <div>
         {Array.from(Array(12), (e, i) => {
           return (
-            <div className={`${styles.hexagon} ${styles['id'+i]}`} key={i}>
-              {i}
+            <div
+              className={`${styles.hexagon} ${
+                OriginAreaId === i + 1 ? styles.active : ""
+              } ${DestinationAreaId === i + 1 ? styles.active : ""} ${
+                styles["id" + i]
+              }`}
+              key={i}
+            >
             </div>
           );
         })}
       </div>
     );
   };
-  return <div className={styles.mapContainer}><Image src={map} alt="map"/>{hexagon()}</div>;
+  return (
+    <div className={styles.mapContainer}>
+      <Image src={map} alt="map" />
+      {hexagon()}
+    </div>
+  );
 };
 
 export default Map;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,60 @@
+"use client";
+import React from "react";
 import Image from "next/image";
 import Map from "./component/map";
 import Eta from "./component/eta";
 import BottomSheet from "./component/BottomSheet";
-
+import { useEffect, useState } from "react";
+const test = {
+  ID: 2,
+  Origin: "منطقه شماره 1 ",
+  Destination: "منطقه شماره 8 ",
+  OriginAreaId: 1,
+  DestinationAreaId: 8,
+  Rate: 5,
+  Eta: 46,
+  Price: 18224,
+};
+interface IOffer {
+  ID: number;
+  Origin: string;
+  Destination: string;
+  OriginAreaId: number;
+  DestinationAreaId: number;
+  Rate: number;
+  Eta: number;
+  Price: number;
+}
 export default function Home() {
+  const [data, setData] = useState<IOffer[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "http://188.121.117.228:8083/offer/list?driverId=6"
+      );
+      const data = await response.json();
+      setData(data.data);
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div>
-      <Map />
-      <Eta />
-      <BottomSheet />
+    <div className="hihi">
+      {data?.map(({ ID, OriginAreaId, DestinationAreaId }) => {
+        return (
+          <div key={ID}>
+            {" "}
+            {ID}
+            <Map
+              OriginAreaId={OriginAreaId}
+              DestinationAreaId={DestinationAreaId}
+            />
+            <Eta />
+            <BottomSheet />
+          </div>
+        );
+      })}
     </div>
   );
 }
-
